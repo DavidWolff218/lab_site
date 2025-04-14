0e5d16ffa69730afa1a0e1f4178b46e1df33cf3e
doodleman218 <dcwolff218@gmail.com>
2025-04-14 13:41:54 -0500

    vite config update

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: [
+      'dw-labs-site-34841808a1f5.herokuapp.com',
+      '.herokuapp.com'
+    ]
+  }
 })
